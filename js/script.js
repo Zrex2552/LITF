25009ed996817327a38253b9869746916dbac679
Vladlens1 <103267342+Vladlens1@users.noreply.github.com>
2024-05-02 21:48:14 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const firstGame = document.querySelector('#first-game-btn');
+
+firstGame.addEventListener('click', flashCards);
+
+function flashCards(event) {
+    alert("Tu esi GEJS! Vai arī ejam atpūsties?")
+}
